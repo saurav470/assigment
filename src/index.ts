51c61d94ad38573f7ed26ad136f50107536340e8
saurav sharma <sauravsharma@sauravs-MacBook-Air.local>
2023-12-17 11:27:34 +0530

    completed

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express ,{Request,Response}from 'express'
+import express, { Request, Response } from 'express'
 import dotenv from "dotenv"
 import cors from 'cors'
 import { connect } from './config/database'
@@ -7,34 +7,34 @@ import userRoutes from './routes/user'
 import studentRoutes from './routes/student'
 
 
-    const app = express();
+const app = express();
 
-    app.use(express.json());
-    dotenv.config()
-    const PORT = Number(process.env.PORT) || 8001;
-    app.use(
-        cors({
-            origin: "*",
-            credentials: true,
-        })
-    )
-    //database connect
-    connect();
-
-    app.get("/health", (req:Request, res:Response) => {
-
-  
-
-        return res.status(200).json({ health: "server is healthy" })
+app.use(express.json());
+dotenv.config()
+const PORT = Number(process.env.PORT) || 8001;
+app.use(
+    cors({
+        origin: "*",
+        credentials: true,
     })
+)
+//database connect
+connect();
 
-    app.use("/api/v1/admin", adminRoutes)
-    app.use("/api/v1/user", userRoutes)
-    app.use("/api/v1/student", studentRoutes)
+app.get("/", (req: Request, res: Response) => {
 
-    app.listen(PORT, () => {
-        console.log(`server is up and running on port ${PORT}`)
-    })
+
+
+    return res.status(200).json({ health: "server is healthy" })
+})
+
+app.use("/api/v1/admin", adminRoutes)
+app.use("/api/v1/user", userRoutes)
+app.use("/api/v1/student", studentRoutes)
+
+app.listen(PORT, () => {
+    console.log(`server is up and running on port ${PORT}`)
+})
 
 
 
